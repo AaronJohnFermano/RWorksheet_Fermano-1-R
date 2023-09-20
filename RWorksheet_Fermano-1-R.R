a87e8b3a25508118bf3ea8ff1e801612008867fd
Aaron John Fermano <JohnAristozaFermano@gmail.com>
2023-09-20 04:04:46 +0000

    new commit

---
+++ RWorksheet_Fermano-1-R.R
@@ -0,0 +1,28 @@
+ageVector <-  c( 34, 28, 34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41, 51, 35, 24, 33, 41.)
+
+length(ageVector)
+
+#2
+reciprocalAge <-1/ageVector
+reciprocalAge 
+
+#3
+new_age <- c(ageVector, 0, ageVector)
+new_age
+
+#4
+SortedageVector <-c(ageVector)
+SortedageVector
+
+#5
+max(ageVector)
+min(ageVector)
+
+#6
+decimalVector <-c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3, 2.5, 2.3, 2.4, 2.7)
+
+length (decimalVector)
+decimalVector *2
+
+#7
+
